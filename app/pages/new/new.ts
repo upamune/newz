d1a4dc5980b57f7f4b098a6fa7d193fd0c1c5630
upamune <jajkeqos@gmail.com>
2016-04-13 01:57:11 +0900

    :rocket: implement new stories

--- app/pages/new/new.ts
+++ app/pages/new/new.ts
@@ -1,7 +1,27 @@
 import {Page} from 'ionic-angular';
-
+import {NewNewsService} from '../../../services/NewNewsService'
+import {NewsItem} from '../../../models/NewsItem'
 
 @Page({
     templateUrl: 'build/pages/new/new.html',
 })
-export class New {}
+export class New {
+    private itemIds: Array<number>;
+    private items : Array<NewsItem>;
+    constructor(private newsService: NewNewsService) {
+        this.items = [];
+
+        this.newsService.fetchItemIds().subscribe(
+            data => {
+                this.itemIds = data;
+                this.itemIds = this.itemIds.slice(0, 30);
+                this.itemIds.map(itemId => {
+                    this.newsService.fetchItem(itemId).subscribe(
+                        data => {
+                            this.items.push(new NewsItem(data));
+                        }
+                    );
+                });
+            });
+    }
+}
--- services/NewNewsService.ts
+++ services/NewNewsService.ts
@@ -3,17 +3,17 @@ import 'rxjs/add/operator/map';
 import {Injectable} from "angular2/core";
 
 @Injectable()
-export class NewsService {
+export class NewNewsService {
 
     constructor(private http: Http) {}
-    
+
     fetchItem(itemId: number) {
         const url = `https://hacker-news.firebaseio.com/v0/item/${itemId}.json`;
         return this.http.get(url).map(res => res.json());
     }
 
     fetchItemIds() {
-        const url = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+        const url = 'https://hacker-news.firebaseio.com/v0/newstories.json';
         return this.http.get(url).map(res => res.json());
     }
 }
